293d387be5fa3ce7e24f60983b89c5a1ccc964b3
ngoducthuan85 <ngoducthuan85@gmail.com>
2015-06-19 01:20:00 +0900

    add mongodb

--- protected/models/RSS.php
+++ protected/models/RSS.php
@@ -19,7 +19,7 @@ class RSS
  	*/
 	function __construct($rssPath)
 	{
-		$m          = new MongoClient(); // connect
+		$m          = new MongoClient('mongodb://ngoducthuan85:ngoducthuan85@ds045242.mongolab.com:45242/livedoor'); // connect
 		$db         = $m->selectDB('livedoor');
 		$collection = $db->selectCollection('news');
 		
