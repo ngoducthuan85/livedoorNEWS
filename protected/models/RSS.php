b1ec862be66e858284dddea1f2ca5245259ee251
ngoducthuan85 <ngoducthuan85@gmail.com>
2015-06-19 05:35:41 +0900

    analyze HTML to get image url and keywords

--- protected/models/RSS.php
+++ protected/models/RSS.php
@@ -95,7 +95,10 @@ class RSS
 	{
 		// <meta name="news_keywords" content="社会,トヨタの女性役員逮捕,密輸,麻薬,厚生労働省,トヨタ自動車,国内の事件・事故,ニュース">
 		preg_match('/<meta name="news_keywords" content="(.*?)">/', $html, $matches);
-		return $matches[1];
+		if ($matches)
+			if (count($matches) > 1)
+				return $matches[1];
+		return ""; 
 	}
 	
 	/**
