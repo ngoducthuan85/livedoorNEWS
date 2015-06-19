7bf64fb2bcd42c11938413f8081cdb40c8a1bc51
ngoducthuan85 <ngoducthuan85@gmail.com>
2015-06-19 09:14:04 +0900

    add article page

--- protected/views/layouts/main2.php
+++ protected/views/layouts/main2.php
@@ -3,7 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=euc-jp" />
-	<meta name="language" content="jp" />
+	<meta name="language" content="en" />
 
 	<!-- blueprint CSS framework -->
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
